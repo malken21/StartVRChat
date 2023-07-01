886bd9b52dc55af57d866945c6583bee5c874e16
malken21 <Malken_Marumasa@outlook.jp>
2023-07-01 19:26:40 +0900

    remove util

--- StartVRC.bat
+++ StartVRC.bat
@@ -15,14 +15,39 @@ rem ----------設定部分---------- end
 
 
 rem SteamVR 起動
-call ./util/task_start.bat %SteamVR% "SteamVR"
+call :task_start %SteamVR% "SteamVR"
 rem SteamVR の起動が完了するまで 待機
-call ./util/task_wait.bat vrmonitor.exe 3
+call :task_wait vrmonitor.exe 3
 
 rem VRChat 起動
-call ./util/task_start.bat %VRChat% "VRChat"
+call :task_start %VRChat% "VRChat"
 rem VRChat の起動が完了するまで 待機
-call ./util/task_wait.bat VRChat.exe 3
+call :task_wait VRChat.exe 3
 
 rem OVR Advanced Settings 起動
-call ./util/task_start.bat %OVR_Advanced_Settings% "OVR Advanced Settings"
+call :task_start %OVR_Advanced_Settings% "OVR Advanced Settings"
+
+rem 処理終了
+exit /b
+
+
+rem ----------関数---------- start
+
+:task_start
+rem "第1引数" を 実行
+start "" %1
+rem コンソールに文字 出力
+echo %~2 Start!!
+exit /b
+
+:task_wait
+rem "第1引数" が 起動するまで 待機
+:loop
+tasklist | find "%1" >nul 2>&1
+if errorlevel 1 (
+    rem "第2引数" 秒ごとに 起動しているか確認
+    timeout /t %2 /nobreak >nul
+    goto loop
+)
+
+rem ----------関数---------- end
--- util/task_start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-start "" %1
-echo %~2 Start!!
